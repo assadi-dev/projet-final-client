9ac8d4e3608b36d68dd4bf39fc6da480d43f8eab
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-11-24 16:07:12 +0100

    suppresion des variable inutilisé

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./assets/css/App.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./router/routes";
