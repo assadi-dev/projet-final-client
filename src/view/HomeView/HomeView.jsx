94c45b5a3bdb971183f9ff6a9a7c6e75993d513a
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-11-24 16:03:02 +0100

    routing des pages public

---
+++ src/view/HomeView/HomeView.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeView = () => {
+  return <div>Accueil</div>;
+};
+
+export default HomeView;
---
+++ src/view/SurveyView/SurveyView.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Survey = () => {
+  return <div>Survey test</div>;
+};
+
+export default Survey;
