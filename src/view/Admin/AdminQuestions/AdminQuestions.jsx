983bd6f77ab23348334fc71b2d6bf32927bb82c9
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-06 21:58:32 +0100

    Update AdminQuestions.jsx

--- src/view/Admin/AdminQuestions/AdminQuestions.jsx
+++ src/view/Admin/AdminQuestions/AdminQuestions.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import DataTable from "../../../components/DataTable/DataTable";
+import { createColumnHelper } from "@tanstack/react-table";
+import useFetchData from "../../../hook/useAdminFetchData";
 
 const AdminQuestions = () => {
-  return <div>AdminQuestions</div>;
+  const { data, isLoading, errors, fetch, abortController } = useFetchData(
+    "/surveys",
+    {}
+  );
+
+  const columnHelper = createColumnHelper();
+
+  const COLUMN = [
+    columnHelper.accessor("id", {
+      header: () => "#",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("title", {
+      header: () => "Sondage",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.display({
+      id: "Action",
+      cell: ({ row }) => <span>icon</span>,
+    }),
+  ];
+
+  useEffect(() => {
+    fetch("/surveys");
+    return () => {
+      abortController.current.abort();
+    };
+  }, []);
+  console.log(data);
+
+  return (
+    <div>
+      <h1> Questionnaire</h1>
+      <DataTable columns={COLUMN} />
+    </div>
+  );
 };
 
 export default AdminQuestions;
