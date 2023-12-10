23987f91dc77fc33a78e234cf7c13bc496e236bb
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-10 20:47:39 +0100

    remplacement useFetchData par useClientFetchData

--- src/view/SurveyView/SurveyView.jsx
+++ src/view/SurveyView/SurveyView.jsx
@@ -2,37 +2,37 @@ import React, { useEffect } from "react";
 import useFetchData from "../../hook/useAdminFetchData";
 import { useParams } from "react-router";
 import QuestionCard from "../../components/question/QuestionCard";
+import useClientFetchData from "../../hook/useClientFetchData";
 
 const Survey = () => {
   // récupération de l'id deu sondage en paramètre
   // à chnager avec un token après
-  const {idSurvey} = useParams()
-  const { data, isLoading, errors, fetch, abortController } = useFetchData(
-    "/client/questions/"+idSurvey,
-    null
-  ); // à remplacer après avec le useClientFetchData
+  const { idSurvey } = useParams();
+  const { data, isLoading, errors, fetch, abortController } =
+    useClientFetchData(); // à remplacer après avec le useClientFetchData
 
-   useEffect(() => {
-    fetch("/client/questions/"+idSurvey, null);
+  useEffect(() => {
+    fetch("/client/questions/" + idSurvey);
     return () => {
-      abortController.current.abort();
+      abortController?.abort();
     };
-  }, [])
+  }, []);
 
   return (
-  <div>
     <div>
-      <h1>Nom du sondage</h1>
-      <p>Description</p>
+      <div>
+        <h1>Nom du sondage</h1>
+        <p>Description</p>
+      </div>
+      <form method="post">
+        {isLoading && <p>Chargement en cours</p>}
+        {data?.data &&
+          data.data.map((question, index) => (
+            <QuestionCard question={question} key={index}></QuestionCard>
+          ))}
+        <button>Envoyer</button>
+      </form>
     </div>
-    <form method="post">
-      {isLoading && <p>Chargement en cours</p>}
-      {data?.data && data.data.map((question, index) => (
-        <QuestionCard question={question} key={index}></QuestionCard>
-      ))}
-      <button>Envoyer</button>
-    </form>
-  </div>
   );
 };
 
