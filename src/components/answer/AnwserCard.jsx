12cd9a90bfc54bfe2d3f447b6aaa03f944d6c67c
Marc AKPOTO-K <brightmarc90@gmail.com>
2023-12-14 23:42:51 +0100

    finalisation affichage des données de réponses

--- src/components/answer/AnwserCard.jsx
+++ src/components/answer/AnwserCard.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const AnwserCard = ({answer}) => {
-  return (
-    <div>AnwserCard</div>
-  )
+    let result = ""
+    if(answer.question.question_type == 'A'){
+        result = <>
+            {answer.question.propositions.map((option) => (
+                (option.proposition == answer.value)? <p key={option.id}>&#11093; {option.proposition}</p> : <p key={option.id}>&#128280; {option.proposition}</p>
+            ))}
+        </>
+    }else{
+        result = <p>{answer.value}</p>
+    }
+    return (
+        <div>
+            <div>
+                <p><span>{answer.question.question_number}</span> <span>{answer.question.question_body}</span></p>
+            </div>
+            <div>
+                {result}
+            </div>
+        </div>
+    )
 }
 
 export default AnwserCard
