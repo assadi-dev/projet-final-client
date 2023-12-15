6d4611b93f81ab16c24cc17585ab1ce687a9ad72
Marc AKPOTO-K <brightmarc90@gmail.com>
2023-12-15 10:18:10 +0100

    styles de la page home

--- src/view/client/HomeView/HomeView.jsx
+++ src/view/client/HomeView/HomeView.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import style from "./home.module.css"
 
 const HomeView = () => {
-  return <div>Accueil</div>;
+  return <div className="d-flex justify-content-center align-items-center h-100 px-3">
+    <div className={`card text-center p-5 ${style.homeCard}`}>
+      <h1 className="mb-4">Bienvenue chez BigScreen</h1>
+      <p className="lead">Nous sommes ravis de vous accueillir sur notre plateforme dédiée à la collecte de vos précieuses opinions.</p>
+      <p className="lead mb-4">En partageant vos pensées à travers ce sondage, vous contribuez à influencer des décisions importantes, services et expériences qui vous concernent directement. Votre voix fait la différence, et chaque réponse compte.</p>
+      <p>{<Link to={"/survey/1"} className="btn btn-primary">Participer au sondage</Link>}</p>
+    </div>
+  </div>
 };
 
 export default HomeView;
