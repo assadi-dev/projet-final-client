9e1170a5f14be2e919d39955ae4089563c35ff57
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-15 11:10:20 +0100

    mise à jour route  login

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -27,10 +27,10 @@ export default createBrowserRouter([
   },
   {
     path: "/answers/:token",
-    element: <AnswerView />
+    element: <AnswerView />,
   },
   {
-    path: "login",
+    path: "administration/login",
     element: <AdminLogin />,
   },
   {
