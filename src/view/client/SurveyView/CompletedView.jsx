1501922428e43d7139cc5864cc5cd49ff05f8152
Marc AKPOTO-K <brightmarc90@gmail.com>
2023-12-15 15:03:07 +0100

    Styles de la page completed

--- src/view/client/SurveyView/CompletedView.jsx
+++ src/view/client/SurveyView/CompletedView.jsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom"
+import style from "./survey.module.css"
 
 const CompletedView = () => {
     const {token} = useParams()
-    return <div>
-            <p>Merci d'avoir participer à ce sondage</p>
-            <p>Vous pourrez consulter ultérieurement vos réponses via ce lien: <a href={`http://localhost:5173/answers/${token}`}>http://localhost:5173/answers/{token}</a></p>
+    return <div className="d-flex justify-content-center align-items-center h-100 px-3">
+        <div className={`card text-center p-5 ${style.completedCard}`}>
+            <h1 className="h2 mb-3">Merci d'avoir participer à ce sondage</h1>
+            <p className="lead">Vous pourrez consulter ultérieurement vos réponses via ce lien : <a href={`http://localhost:5173/answers/${token}`}>http://localhost:5173/answers/{token}</a></p>
         </div>
+    </div>
 }
 
 export default CompletedView
