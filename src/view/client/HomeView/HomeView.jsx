2f9fa76cbfe80df01fac1a3662f32106b91a2a09
Marc AKPOTO-K <brightmarc90@gmail.com>
2023-12-15 10:20:06 +0100

    Modification de texte de la home

--- src/view/client/HomeView/HomeView.jsx
+++ src/view/client/HomeView/HomeView.jsx
@@ -7,7 +7,7 @@ const HomeView = () => {
     <div className={`card text-center p-5 ${style.homeCard}`}>
       <h1 className="mb-4">Bienvenue chez BigScreen</h1>
       <p className="lead">Nous sommes ravis de vous accueillir sur notre plateforme dédiée à la collecte de vos précieuses opinions.</p>
-      <p className="lead mb-4">En partageant vos pensées à travers ce sondage, vous contribuez à influencer des décisions importantes, services et expériences qui vous concernent directement. Votre voix fait la différence, et chaque réponse compte.</p>
+      <p className="lead mb-4">En partageant vos pensées à travers ce sondage, vous contribuez à influencer des décisions importantes. Votre voix fait la différence, et chaque réponse compte.</p>
       <p>{<Link to={"/survey/1"} className="btn btn-primary">Participer au sondage</Link>}</p>
     </div>
   </div>
