946c6cec4ccc6fa2b879523de624e5e46013401f
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-18 01:33:48 +0100

    Ajustement icon sidebar

--- src/view/layout/AdminLayout/AdminSidebar.jsx
+++ src/view/layout/AdminLayout/AdminSidebar.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import Bigscreen from "../../../components/Svg/Bigscreen";
+import { FaHome } from "react-icons/fa";
+import { FaPersonCircleQuestion } from "react-icons/fa6";
+import { FaListCheck } from "react-icons/fa6";
 
 const AdminSidebar = () => {
   const sidebarNavigation = [
     {
       name: "Accueil",
       to: "/administration",
+      icon: FaHome,
     },
     {
       name: "Questions",
       to: "/administration/questions",
+      icon: FaPersonCircleQuestion,
     },
     {
       name: "Reponse",
       to: "/administration/answers",
+      icon: FaListCheck,
     },
   ];
 
@@ -32,7 +38,7 @@ const AdminSidebar = () => {
             return (
               <li key={item.name} className="sidebar-item">
                 <NavLink className="sidebar-link" to={item.to}>
-                  <i className="align-middle" data-feather="sliders"></i>{" "}
+                  <item.icon />
                   <span className="align-middle">{item.name}</span>
                 </NavLink>
               </li>
