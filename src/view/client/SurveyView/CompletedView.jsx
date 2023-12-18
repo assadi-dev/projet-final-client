e59494d607a917895c9fe2cfec7474b9afa6fc39
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-18 02:40:51 +0100

    correction affichage liens des reponses des participants

--- src/view/client/SurveyView/CompletedView.jsx
+++ src/view/client/SurveyView/CompletedView.jsx
@@ -1,14 +1,21 @@
-import { Link, useParams } from "react-router-dom"
-import style from "./survey.module.css"
+import { Link, useParams } from "react-router-dom";
+import style from "./survey.module.css";
 
 const CompletedView = () => {
-    const {token} = useParams()
-    return <div className="d-flex justify-content-center align-items-center h-100 px-3">
-        <div className={`card text-center p-5 ${style.completedCard}`}>
-            <h1 className="h2 mb-3">Merci d'avoir participer à ce sondage</h1>
-            <p className="lead">Vous pourrez consulter ultérieurement vos réponses via ce lien : <Link to={`/answers/${token}`}>http://localhost:5173/answers/{token}</Link></p>
-        </div>
-    </div>
-}
+  const { token } = useParams();
+  const URL = `${window.location.protocol}//${window.location.host}/answers/${token}`;
 
-export default CompletedView
+  return (
+    <div className="d-flex justify-content-center align-items-center h-100 px-3">
+      <div className={`card text-center p-5 ${style.completedCard}`}>
+        <h1 className="h2 mb-3">Merci d'avoir participer à ce sondage</h1>
+        <p className="lead">
+          Vous pourrez consulter ultérieurement vos réponses via ce lien :{" "}
+          <Link to={`/answers/${token}`}>{URL}</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CompletedView;
